cc7d44ea2d179d32a409e9a3b375b1383182b7ad
Mehdi-Bendriss <bendrissmehdi@gmail.com>
2022-08-12 10:51:16 +0200

    Fixed organize paths

--- wrappers/start.sh
+++ wrappers/start.sh
@@ -14,3 +14,9 @@ then
   echo y | bin/opensearch-plugin install "${SECURITY_PLUGIN_URL}"
 fi
 
+# start
+"${SNAP}"/usr/bin/setpriv \
+  --clear-groups \
+  --reuid snap_daemon \
+  --regid snap_daemon -- \
+  "${OPENSEARCH_PATH_CONF}" "${SNAP_DATA}"/bin/opensearch

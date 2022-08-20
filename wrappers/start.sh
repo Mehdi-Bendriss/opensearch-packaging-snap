e122fd64b7f0a46256e18426f210dc9b87496c7a
Mehdi-Bendriss <bendrissmehdi@gmail.com>
2022-08-20 22:11:28 +0200

    Added install and configure hooks + fix bug not setting property

--- wrappers/start.sh
+++ wrappers/start.sh
@@ -2,21 +2,29 @@
 
 set -eux
 
-DAEMON_SYSTEMD_PATH="/etc/systemd/system/snap.opensearch.daemon.service.d"
-script_log="${DAEMON_SYSTEMD_PATH}"/start.log
-exec 1>>$script_log
-exec 2>&1
+
+function snap_logging () {
+    SNAP_ACTIONS_LOG_DIR="/etc/systemd/system/snap.${PROJECT_NAME}.daemon.service.d/logs"
+
+    script_log="${SNAP_ACTIONS_LOG_DIR}/$1.log"
+    exec 1>>"${script_log}"
+    exec 2>&1
+}
 
 
 function disable_security_plugin () {
     key="plugins.security.disabled:"
     conf_file="${OPENSEARCH_PATH_CONF}/opensearch.yml"
 
-    if ! grep -q "^${key}" "${conf_file}";
+    if grep -q "^${key}" "${conf_file}";
     then
-        sed -i "s@.*${key}.*@${key}: true@" "${conf_file}"
+        sed -i "s@.*${key}.*@${key} true@" "${conf_file}"
+    else
+        echo "${key} true" >> "${conf_file}"
     fi
 }
+
+snap_logging "service-start"
 
 disable_security_plugin
 
